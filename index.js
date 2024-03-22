befe6cc33877da8390a72abb7edf7a8d3fed0d0a
Saimon <saimon22011@gmail.com>
2024-03-22 22:01:54 +0400

    fixed versions

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // <== Will be created later
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200; // <== You can change the port
+const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
